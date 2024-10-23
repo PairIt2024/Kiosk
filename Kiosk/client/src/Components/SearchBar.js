4f6568346e2f8491ac60526a9930fb001639cc93
Owen Hall <owen9hall@gmail.com>
2024-10-23 00:03:50 -0700

    removed endpoint from SearchBar implementation

--- Kiosk/client/src/Components/SearchBar.js
+++ Kiosk/client/src/Components/SearchBar.js
@@ -10,35 +10,12 @@ export default function SearchBar({ onSearch }) {
     if (onSearch) {
       onSearch(query);
     }
-    console.log("Search query:", query); // TEMPORARY: Checks what query is
-    // Example of sending the query to a backend as a plain string
-    try {
-        const response = await fetch('https://REPLACE-ME-WITH-BACKEND-ENDPOINT.com/search', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'text/plain', // tell the server to expect a plain string
-          },
-          body: query, // Send the query as a plain string
-        });
-  
-        if (!response.ok) { // checks if response status is in range 200-299 (error if not)
-          throw new Error('Network response was not ok');
-        }
-  
-        const data = await response.json();
-        console.log('Search results:', data);
-      } 
-      catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-      }
   };
-
-  
 
 return (
     <Box className="search-bar-container">
         <TextField
-            value={query}
+            value={query} // query holds user's text input
             onChange={(e) => setQuery(e.target.value)} // Updates query to hold user text input when pressing search button
             placeholder="Enter class information..."
             variant="outlined"
